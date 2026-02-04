dde7809b22df489907f4284d51e238db7a4b63a2
Dumpala Rushik <dumpala.rushik@mindfiresolutions.com>
2026-02-05 00:33:51 +0530

    feat: add swagger documentation integration

---
+++ src/swagger/swagger.ts
@@ -0,0 +1,41 @@
+import swaggerJsdoc from 'swagger-jsdoc';
+
+const swaggerOptions: swaggerJsdoc.Options = {
+  definition: {
+    openapi: '3.0.3',
+    info: {
+      title: 'Subscription & Billing System API',
+      version: '1.0.0',
+      description:
+        'API documentation for Assignment 5 - Subscription & Billing System'
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000'
+      }
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT'
+        }
+      }
+    },
+    security: [
+      {
+        bearerAuth: []
+      }
+    ]
+  },
+
+  apis: [
+    './src/routes/*.ts',
+    './src/controller/*.ts'
+  ]
+};
+
+const swaggerSpec = swaggerJsdoc(swaggerOptions);
+
+export default swaggerSpec;
